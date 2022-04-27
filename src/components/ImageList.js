4378092766f825fd99093ed80fb864187060fe69
Juan Carlos <2172742@my.ipleira.pt>
2022-04-27 15:52:18 +0100

    Extract new component for the images

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,11 +1,12 @@
 import './ImageList.css'
 import React from "react";
+import ImageCard from "./ImageCard";
 
 const ImageList = props => {
     // const images = props.images.map((image) => {
     //     return <img src={image.urls.regular}/>
     // });
-    const images = props.images.map(({description, id, urls}) => <img key={id} src={urls.regular} alt={description}/>);
+    const images = props.images.map((image) => <ImageCard key={image.id} image={image}/>);
     console.log(props.images);
     return (
         <div className="image-list">
