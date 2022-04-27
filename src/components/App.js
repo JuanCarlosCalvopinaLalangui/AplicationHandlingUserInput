227c9fe0511cf37571a4010799787d9057f183f7
Juan Carlos <2172742@my.ipleira.pt>
2022-04-27 14:21:15 +0100

    Use axios to get some images using API unsplash.com
    we make a status to take information response from axios and make async function

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,35 @@
 import React from "react";
+import axios from "axios";
 import SearchBar from "./SearchBar";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-    state = { term : '' }
+    state = { images: [] }
 
-    onSearchSubmit(term){
-        console.log(term);
+    // async means u must use await in the function
+    onSearchSubmit = async (term) => {
+        const response = await axios.get('https://api.unsplash.com/search/photos', {
+            params: {query: term},
+            headers: {
+                Authorization: 'Client-ID 8w_f_pTRzbaCO2aHL3L4YJd-cKtfJdNVGxnk88ZaWkg'
+            }
+        });
+        // }).then((response) => {
+        //     console.log(response.data.results);
+        // });
+
+        this.setState({
+            images: response.data.results
+        })
+        // console.log(response.data.results);
     }
 
     render() {
         return (
             <div className="ui container" style={{marginTop: '10px'}}>
                 <SearchBar runOnSubmit={this.onSearchSubmit}/>
+                Found: {this.state.images.length}
+                {/*<ImageList images={this.state.images} />*/}
             </div>
         );
     }
