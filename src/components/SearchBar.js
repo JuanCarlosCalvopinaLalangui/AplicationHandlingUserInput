281a33300d8d17948bb3dd948032757b80cef414
Juan Carlos <2172742@my.ipleira.pt>
2022-04-27 10:58:25 +0100

    Creating Event Handler
    capturing this events using event.target.value

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+    onInputChange(event){ // callback function used in input tag typical named function on[in what object][Change/Click/Submit]
+        console.log(event.target.value);
+    }
+
     render() {
         return (
             <div className="ui segment">
                 <form className="ui form">
                     <div className="field">
                         <label>Image Search</label>
-                        <input type="text"/>
+                        <input type="text" onChange={this.onInputChange}/>
                     </div>
 
                 </form>
